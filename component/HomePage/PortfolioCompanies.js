05f10d235fb96a6921e0da8e8458a33d71272a3c
Emy Ifeora <emyboybeats@gmail.com>
2023-02-07 21:33:09 +0100

    ..

--- component/HomePage/PortfolioCompanies.js
+++ component/HomePage/PortfolioCompanies.js
@@ -6,30 +6,38 @@ export default function PortfolioCompanies() {
 		<section>
 			<div className="container pb-2">
 				<small className="text-theme">PORTFOLIO</small>
-				<h3 className='text-theme-dark'>Our Portfolio Companies</h3>
+				<h3 className="text-theme-dark">Our Portfolio Companies</h3>
 				<div className="d-flex my-5" style={{ overflowX: 'scroll' }}>
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
-					<EachCompany />
+					<EachCompany
+						title={`Infrastructure & Services`}
+						img={`https://media.istockphoto.com/id/1297780288/photo/delivering-quality-construction-for-a-quality-lifestyle.jpg?b=1&s=170667a&w=0&k=20&c=Mdr_fFyIHi2jBYU2ns-Fs2JlYPk0QdP9oK3UW6jhayM=`}
+					/>
+					<EachCompany
+						title={`Financial Services`}
+						img={`https://www.theforage.com/blog/wp-content/uploads/2022/07/finance-careers.jpg`}
+					/>
+					<EachCompany
+						title={`Energy & Extractives`}
+						img={`https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmluYW5jZXxlbnwwfHwwfHw%3D&w=1000&q=80`}
+					/>
+					<EachCompany
+						title={`Something Else Here`}
+						img={`https://mezcredit.com/wp-content/uploads/2020/01/Business-Finance-1.jpg`}
+					/>
 				</div>
 			</div>
 		</section>
 	)
 }
 
-const EachCompany = () => {
+const EachCompany = ({ title, img }) => {
 	return (
 		<div
 			className="card bg-dark mx-3 bg-img image-1 overlay-anim wow fadeInUp animated"
 			style={{
 				minWidth: '300px',
 				height: '430px',
-				backgroundImage: `url(https://i0.wp.com/stanzaliving.wpcomstaging.com/wp-content/uploads/2022/04/5ba1a-top-companies-in-india.jpg?fit=1000%2C665&ssl=1)`,
+				backgroundImage: `url(${img})`,
 			}}
 		>
 			<div
@@ -42,9 +50,14 @@ const EachCompany = () => {
 					right: 0,
 				}}
 			/>
-			<div className="card-body d-flex flex-column justify-content-end" style={{zIndex: 10}}>
-				<p className='text-white fw-bold'>Infrastructure & Services</p>
-				<p className='text-white fw-light'>Explore <ArrowRight color={'white'} /></p>
+			<div
+				className="card-body d-flex flex-column justify-content-end"
+				style={{ zIndex: 10 }}
+			>
+				<p className="text-white fw-bold">{title}</p>
+				<p className="text-white fw-light">
+					Explore <ArrowRight color={'white'} />
+				</p>
 			</div>
 		</div>
 	)
